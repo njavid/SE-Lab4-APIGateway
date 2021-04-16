19457688a2689d92a02d26e954410a7aab938682
Ehyaee <z.ehyaee96@gmail.com>
2021-04-16 23:35:09 +0430

    impelement login[C api in gateway

--- APIGateway/gateway/views.py
+++ APIGateway/gateway/views.py
@@ -100,6 +100,61 @@ def register(request):
         response_data['message'] = 'Service not available'
         return HttpResponse(json.dumps(response_data), content_type="application/json", status=503)
 
+@csrf_exempt
+def login(request):
+    stop =  cache.get('stop')
+
+    counter = request.session.get('fail_counter')
+
+    if counter is None:
+        counter = 0
     
+    if stop is None:
+        response_data = {}
+        try:
+            received_json_data = json.loads(request.body)
+            
+            data = {
+                'username' : received_json_data["username"],
+                'password' : received_json_data["password"],
+        
+            }
+            print(data)
+            request_type = "GET"
+            api_url = 'http://127.0.0.1:3000/login/'
+            response = requests.request(request_type, api_url, data=json.dumps(data), timeout=0.5)
+        
+
+            print("status=",response.status_code)
+            if response.status_code == 200:
+                response_data['message'] = 'logged in successfully'
+                return HttpResponse(json.dumps(response_data), content_type="application/json", status=200)
+            elif response.status_code == 400:
+                response_data['message'] = 'username or password incorrect!'
+                return HttpResponse(json.dumps(response_data), content_type="application/json", status=401)
+            else:
+                response_data['message'] = 'method not allowed'
+                return HttpResponse(json.dumps(response_data), content_type="application/json", status=405)
+
+
+
+        except Exception as e:
+            print(e)
+            counter += 1
+            if counter % 3 == 0:
+                cache.set('stop', 'stop', 30)
+
+            request.session['fail_counter'] = counter
+
+            response_data['message'] = 'Timeout! Please try again.'
+            return HttpResponse(json.dumps(response_data), content_type="application/json", status=408)
+
+    else:
+        response_data['message'] = 'Service not available'
+        return HttpResponse(json.dumps(response_data), content_type="application/json", status=503)
+
+    
+
+ 
 
 
