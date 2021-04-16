9ecf99ae645aab6d4ffa0ee2bce7aeab93392462
Ehyaee <z.ehyaee96@gmail.com>
2021-04-16 23:26:59 +0430

    impelement register api in gateway

--- APIGateway/gateway/views.py
+++ APIGateway/gateway/views.py
@@ -9,10 +9,10 @@ from django.core.cache import cache
 def index(request):
 #    del request.session['fail_counter']
 
-    print("start")
+    #print("start")
     
     stop =  cache.get('stop')
-    print(stop)
+    #print(stop)
     counter = request.session.get('fail_counter')
 
     if counter is None:
@@ -24,20 +24,82 @@ def index(request):
             data = request.POST
             res = requests.post(external_api_url, data, timeout=0.5)
 
+
         except Exception as e:
-            print("counter :",counter % 3)
+            #print("counter :",counter % 3)
             counter += 1
             if counter % 3 == 0:
-                print("hererer")
+                #print("hererer")
                 cache.set('stop', 'stop', 30)
 
             request.session['fail_counter'] = counter
-            print("Timeout", counter)
+
+            response_data = {}
+            response_data['message'] = 'Timeout! Please try again.'
+            HttpResponse(json.dumps(response_data), content_type="application/json", status=408)
+
+
     else:
-        print("NOT availble")
+        response_data = {}
+        response_data['message'] = 'Service not available'
+        HttpResponse(json.dumps(response_data), content_type="application/json", status=503)
+        
+
+    #return HttpResponse("Hello, world. You're at the gateway index.")
+
+@csrf_exempt
+def register(request):
+    stop =  cache.get('stop')
+
+    counter = request.session.get('fail_counter')
+
+    if counter is None:
+        counter = 0
+    
+    if stop is None:
+        response_data = {}
+        try:
+            received_json_data = json.loads(request.body)
+            
+            data = {
+                'username' : received_json_data["username"],
+                'password' : received_json_data["password"],
+                'email' : received_json_data["email"],
+                'mobile' : received_json_data["mobile"],
+            }
+            print(data)
+            request_type = "POST"
+            api_url = 'http://127.0.0.1:3000/register/'
+            response = requests.request(request_type, api_url, data=json.dumps(data), timeout=0.5)
+        
+
+            print("status=",response.status_code)
+            if response.status_code == 201:
+                response_data['message'] = 'created successfully'
+                return HttpResponse(json.dumps(response_data), content_type="application/json", status=201)
+            elif response.status_code == 400:
+                response_data['message'] = 'username exists!'
+                return HttpResponse(json.dumps(response_data), content_type="application/json", status=400)
+            else:
+                response_data['message'] = 'method not allowed'
+                return HttpResponse(json.dumps(response_data), content_type="application/json", status=405)
+
+
+
+        except Exception as e:
+            counter += 1
+            if counter % 3 == 0:
+                cache.set('stop', 'stop', 30)
+
+            request.session['fail_counter'] = counter
+
+            response_data['message'] = 'Timeout! Please try again.'
+            return HttpResponse(json.dumps(response_data), content_type="application/json", status=408)
+
+    else:
+        response_data['message'] = 'Service not available'
+        return HttpResponse(json.dumps(response_data), content_type="application/json", status=503)
 
     
-    
-    print("success")
-    
-    return HttpResponse("Hello, world. You're at the gateway index.")
+
+
