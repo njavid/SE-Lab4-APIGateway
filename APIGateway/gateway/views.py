c569326eaefb912ade34f9a84d5236cd40b9cbbb
Ehyaee <z.ehyaee96@gmail.com>
2021-04-17 00:21:53 +0430

    add token to login response

--- APIGateway/gateway/views.py
+++ APIGateway/gateway/views.py
@@ -4,49 +4,6 @@ import json
 import requests
 from django.core.cache import cache
 
-
-@csrf_exempt
-def index(request):
-#    del request.session['fail_counter']
-
-    #print("start")
-    
-    stop =  cache.get('stop')
-    #print(stop)
-    counter = request.session.get('fail_counter')
-
-    if counter is None:
-        counter = 0
-    
-    if stop is None:
-        try:
-            external_api_url = 'http://127.0.0.1:3000/here/'
-            data = request.POST
-            res = requests.post(external_api_url, data, timeout=0.5)
-
-
-        except Exception as e:
-            #print("counter :",counter % 3)
-            counter += 1
-            if counter % 3 == 0:
-                #print("hererer")
-                cache.set('stop', 'stop', 30)
-
-            request.session['fail_counter'] = counter
-
-            response_data = {}
-            response_data['message'] = 'Timeout! Please try again.'
-            HttpResponse(json.dumps(response_data), content_type="application/json", status=408)
-
-
-    else:
-        response_data = {}
-        response_data['message'] = 'Service not available'
-        HttpResponse(json.dumps(response_data), content_type="application/json", status=503)
-        
-
-    #return HttpResponse("Hello, world. You're at the gateway index.")
-
 @csrf_exempt
 def register(request):
     stop =  cache.get('stop')
@@ -125,9 +82,10 @@ def login(request):
             response = requests.request(request_type, api_url, data=json.dumps(data), timeout=0.5)
         
 
-            print("status=",response.status_code)
+            print("status=",response.status_code, json.loads(response.text)['token'])
             if response.status_code == 200:
                 response_data['message'] = 'logged in successfully'
+                response_data['token'] = json.loads(response.text)['token']
                 return HttpResponse(json.dumps(response_data), content_type="application/json", status=200)
             elif response.status_code == 400:
                 response_data['message'] = 'username or password incorrect!'
